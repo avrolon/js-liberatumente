0e6e5ae39906c9aa51f461023ec6c66cc7594260
Andrea Vanina Rolon <andreavarolon@gmail.com>
2022-04-19 15:27:14 -0300

    se realizan modificaciones

--- js/notas.js
+++ js/notas.js
@@ -37,11 +37,11 @@ function displayNotes() {
     notesObj = JSON.parse(notesString);
   }
 
-  let pant = "";
+  let add = "";
 
   notesObj.forEach(function (element, index) {
-    pant += `
-				<div class="card mx-4 my-2 bg-light text-dark thatsMyNote" style="width: 18rem;">
+    add += `
+				<div class="card mx-4 my-2 bg-light text-dark" style="width: 18rem;">
 					<div class="card-body">
 						<h6>${element.time}</h6>
 						<p class="card-text">${element.text
@@ -56,7 +56,7 @@ function displayNotes() {
   let noteEle = document.getElementById("notes");
 
   if (notesObj.length != 0) {
-    noteEle.innerHTML = pant;
+    noteEle.innerHTML = add;
   } else {
     noteEle.innerHTML =
       '<h3 style="text-align: center; color: grey;">Aún no agregaste ninguna nota...</h3>';
@@ -78,38 +78,3 @@ function deleteNote(index) {
 
   displayNotes();
 }
-
-let search = document.getElementById("search");
-search.addEventListener("input", function (e) {
-  let inputText = search.value;
-
-  if (inputText == "") {
-    document.getElementById("noMatches").innerHTML = "";
-  }
-
-  var countNone = 0;
-
-  let cards = document.getElementsByClassName("thatsMyNote");
-
-  Array.from(cards).forEach(function (ele) {
-    let cardText = ele.getElementsByTagName("p")[0].innerText;
-    if (cardText.includes(inputText)) {
-      ele.style.display = "block";
-    } else {
-      ele.style.display = "none";
-
-      countNone++;
-
-      if (countNone === cards.length) {
-        document.getElementById("noMatches").innerHTML =
-          '<h3 style="text-align: center; color: grey;">No matches found</h3>';
-      } else {
-        document.getElementById("noMatches").innerHTML = "";
-      }
-    }
-  });
-
-  if (countNone === 0) {
-    document.getElementById("noMatches").innerHTML = "";
-  }
-});
